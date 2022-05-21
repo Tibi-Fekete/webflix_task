42e16d6f1b1c6228a9c6166d86e26f8b1c211d94
Tibi-Fekete <fekete.tibor987@gmail.com>
2022-05-21 16:57:40 +0200

    add & style header, create webflix logo

--- webflix/src/app/app.module.ts
+++ webflix/src/app/app.module.ts
@@ -10,12 +10,15 @@ import { ResultListComponent } from './components/pages/search-page/result-list/
 import {CardModule} from 'primeng/card';
 import {PaginatorModule} from 'primeng/paginator';
 import {DialogModule} from 'primeng/dialog';
+import {MenubarModule} from 'primeng/menubar';
 import {InputTextModule} from 'primeng/inputtext';
+import {ImageModule} from 'primeng/image';
 import {TableModule} from 'primeng/table';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { SearchPanelComponent } from './components/pages/search-page/search-panel/search-panel.component';
 import { NoResultsComponent } from './components/pages/search-page/no-results/no-results.component';
+import { HeaderComponent } from './components/common/header/header.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,8 @@ import { NoResultsComponent } from './components/pages/search-page/no-results/no
     DetailsModalComponent,
     ResultListComponent,
     SearchPanelComponent,
-    NoResultsComponent
+    NoResultsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +39,9 @@ import { NoResultsComponent } from './components/pages/search-page/no-results/no
     PaginatorModule,
     DialogModule,
     TableModule,
-    InputTextModule
+    InputTextModule,
+    MenubarModule,
+    ImageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
